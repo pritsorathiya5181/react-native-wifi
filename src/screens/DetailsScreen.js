1e375ecfb56fb25955f8065320a09fa772e68998
Darshan Togadiya <togadia.darshan2784@gmail.com>
2020-09-28 22:34:05 +0530

    darshan_commit

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -4,6 +4,11 @@ import React from 'react';
 import { WebView } from "react-native-webview";
 
 const DetailsScreen = ({ navigation }) => {
+
+    state = {
+        isModalVisible: false,
+    }
+
     const webviewRef = React.useRef(null);
     // function webViewgoback() {
     //     if (webviewRef.current) webviewRef.current.goBack();
@@ -23,6 +28,19 @@ const DetailsScreen = ({ navigation }) => {
             />
         );
     }
+
+    toggleModal = () => {
+        this.setState({ isModalVisible: !this.state.isModalVisible });
+        // console.log(this.state.ssid, this.state.password);
+        wifi.connectToHiddenNetwork(this.state.ssid, this.state.password, (networkAdded) => {
+            // console.log(networkAdded);
+            if (networkAdded) {
+                console.log("Connected successfully!");
+            } else {
+                console.log("Connection failed!");
+            }
+        })
+    };
 
     return (
         <>
@@ -45,6 +63,76 @@ const DetailsScreen = ({ navigation }) => {
                     </TouchableOpacity>
                 </View>
             </SafeAreaView>
+
+            <TouchableOpacity onPress={this.toggleModal}
+                style={{ position: "absolute", bottom: 50, right: 30 }}>
+                <LinearGradient
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 0 }}
+                    colors={['#4c669f', '#3b5998', '#192f6a']}
+                    style={styles.linearGradient}>
+                    <Text style={styles.buttonText}>
+                        +
+          </Text>
+                </LinearGradient>
+            </TouchableOpacity>
+
+            <Modal
+                isVisible={this.state.isModalVisible}>
+                <View style={{ backgroundColor: "#DCDCDC", justifyContent: 'center', alignItems: 'center', borderRadius: 20 }}>
+
+                    <View style={{ marginVertical: 15, alignItems: "center" }}>
+                        <Text style={{ fontSize: 20 }}>Enter Wifi Details:</Text>
+                    </View>
+
+                    <View style={styles.inputContainer}>
+                        <Image style={styles.inputIcon} source={{ uri: 'https://png.icons8.com/message/ultraviolet/50/3498db' }} />
+                        <TextInput style={styles.inputs}
+                            placeholder="SSID"
+                            keyboardType='default'
+                            underlineColorAndroid='transparent'
+                            onChangeText={(ssid) => this.setState({ ssid })} />
+                    </View>
+
+                    <View style={styles.inputContainer}>
+                        <Image style={styles.inputIcon} source={{ uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db' }} />
+                        <TextInput style={styles.inputs}
+                            placeholder="Password"
+                            secureTextEntry={true}
+                            underlineColorAndroid='transparent'
+                            onChangeText={(password) => this.setState({ password })} />
+                    </View>
+
+                    <TouchableOpacity onPress={this.toggleModal}
+                        style={{ width: "100%", alignItems: "center", marginVertical: 15 }}>
+                        <LinearGradient
+                            start={{ x: 0, y: 0 }}
+                            end={{ x: 1, y: 0 }}
+                            colors={['#4c669f', '#3b5998', '#192f6a']}
+                            style={{
+                                paddingLeft: 15,
+                                paddingRight: 15,
+                                paddingVertical: 10,
+                                borderRadius: 50,
+                                width: "50%",
+                            }}>
+                            <Text style={{
+                                fontSize: 20,
+                                fontFamily: 'Gill Sans',
+                                textAlign: 'center',
+                                color: '#ffffff',
+                                backgroundColor: 'transparent',
+                            }}>
+                                Close
+          </Text>
+                        </LinearGradient>
+                    </TouchableOpacity>
+
+
+                </View>
+            </Modal>
+
+
         </>
     );
 }
@@ -77,4 +165,69 @@ const styles = StyleSheet.create({
         width: 20,
         height: 20,
     },
+    instructionsContainer: {
+        padding: 15,
+        borderBottomWidth: 1,
+        borderBottomColor: '#CCCCCC',
+    },
+    instructionsTitle: {
+        marginBottom: 10,
+        color: '#333333',
+        fontWeight: "bold",
+        fontSize: 21,
+        letterSpacing: 1.5,
+    },
+    linearGradient: {
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderRadius: 30,
+        width: 60,
+        height: 60,
+    },
+    buttonText: {
+        marginTop: 10,
+        fontSize: 30,
+        fontFamily: 'Gill Sans',
+        textAlign: 'center',
+        color: '#ffffff',
+        backgroundColor: 'transparent',
+    },
+    inputContainer: {
+        borderBottomColor: '#F5FCFF',
+        backgroundColor: '#FFFFFF',
+        borderRadius: 30,
+        borderBottomWidth: 1,
+        width: 250,
+        height: 45,
+        marginVertical: 15,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    inputs: {
+        height: 45,
+        marginLeft: 16,
+        borderBottomColor: '#FFFFFF',
+        flex: 1,
+    },
+    inputIcon: {
+        width: 30,
+        height: 30,
+        marginLeft: 15,
+        justifyContent: 'center'
+    },
+    buttonContainer: {
+        height: 45,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+        width: 250,
+        borderRadius: 30,
+    },
+    loginButton: {
+        backgroundColor: "#00b5ec",
+    },
+    loginText: {
+        color: 'white',
+    }
 });
